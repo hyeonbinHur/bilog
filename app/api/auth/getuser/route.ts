cee83f78aff8e64c6e7ade30b48233d6bbebb1b9
Hur Hyeon Bin (Max) <160996936+hyeonbinHur@users.noreply.github.com>
2024-12-14 03:16:45 +0900

    FEAT: comment patch api"

--- app/api/auth/getuser/route.ts
+++ app/api/auth/getuser/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse, NextRequest } from "next/server";
 import { executeQuery } from "@/lib/mysqlClient";
+import handleError from "@/helper/apiUtils";
 
 export async function GET(req: NextRequest) {
   const email = req.nextUrl.searchParams.get("email");
@@ -9,22 +10,12 @@ export async function GET(req: NextRequest) {
   try {
     const sql = "SELECT * FROM User WHERE email = ?";
     const result = await executeQuery(sql, [email]);
-
     // `result`가 배열인지 확인 후 접근
     if (!Array.isArray(result) || result.length === 0) {
       return NextResponse.json({ error: "User not found" }, { status: 401 });
     }
-
     return NextResponse.json(result[0]); // 특정 유저 정보 반환
   } catch (err) {
-    console.error(err);
-    if (err instanceof Error) {
-      return NextResponse.json({ error: err.message }, { status: 500 });
-    } else {
-      return NextResponse.json(
-        { error: "An unknown error occurred" },
-        { status: 500 }
-      );
-    }
+    return handleError(err);
   }
 }
--- app/api/comment/[id]/route.ts
+++ app/api/comment/[id]/route.ts
@@ -1,24 +1,27 @@
 import { NextRequest, NextResponse } from "next/server";
 import { executeQuery } from "@/lib/mysqlClient";
 import handleError from "@/helper/apiUtils";
-
 interface Props {
   id: string;
 }
-
 export async function PATCH(req: NextRequest, { params }: { params: Props }) {
   try {
-    const { content, user_id, like, dislike } = await req.json(); // 요청 본문에서 content 가져오기
     if (!params.id) {
       throw new Error("comment id is required");
     }
+    const { content }: { content?: string } = await req.json();
+    if (!content || content.length === 0) {
+      throw new Error("comment must includes content");
+    }
     const sql = "UPDATE Comment SET content = ? WHERE comment_id = ?";
+    const values = [content, params.id];
+    const result = await executeQuery(sql, values);
+    return NextResponse.json(result, { status: 200 });
   } catch (err) {
     console.error(err);
     return handleError(err);
   }
 }
-
 export async function DELETE(req: NextRequest, { params }: { params: Props }) {
   try {
     const sql = "DELETE FROM Comment WHERE comment_id = ?";
