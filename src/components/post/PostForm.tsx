e5805c86c9e601ffdf03d0e9dfb2c8e90767284a
Hur Hyeon Bin (Max) <160996936+hyeonbinHur@users.noreply.github.com>
2025-04-02 15:13:46 +0900

    bug fix - While editting post first rendering, the post category was not appeared / fix done

--- src/components/post/PostForm.tsx
+++ src/components/post/PostForm.tsx
@@ -120,112 +120,111 @@ const PostForm = ({ post, lang }: { post?: IPost; lang: string }) => {
       }
     }
   };
+useEffect(() => {
+  console.log("Initial category_id:", getValues("category_id"));
+}, []);
 
-  useEffect(() => {
-    if (post?.thumbnail) {
-      // post에서 thumbnail을 받아왔을 경우 미리보기 설정
-      setImage(post.thumbnail);
+useEffect(() => {
+  if (post?.thumbnail) {
+    // post에서 thumbnail을 받아왔을 경우 미리보기 설정
+    setImage(post.thumbnail);
+  }
+}, [post?.thumbnail]);
+
+useEffect(() => {
+  const fetchCategories = async () => {
+    const categoryResponse = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/category?type=${type}`,
+      { next: { tags: [`category-${type}`] } }
+    );
+    if (!categoryResponse.ok) {
+      return <div>error</div>;
     }
-  }, [post?.thumbnail]);
+    const result = await categoryResponse.json();
+    setCategories(result);
+  };
+  fetchCategories();
+}, []);
 
-  useEffect(() => {
-    const fetchCategories = async () => {
-      const categoryResponse = await fetch(
-        `${process.env.NEXT_PUBLIC_BASE_URL}/category?type=${type}`,
-        { next: { tags: [`category-${type}`] } }
-      );
-      if (!categoryResponse.ok) {
-        return <div>error</div>;
-      }
-      const result = await categoryResponse.json();
-      setCategories(result);
-    };
-    fetchCategories();
-  }, []);
+return (
+  <div>
+    <form
+      className="flex flex-col gap-6 mb-52"
+      onSubmit={onSubmit(handleSubmit)}
+    >
+      <section>
+        <Label>Title</Label>
+        <Input
+          type="text"
+          placeholder="Title"
+          {...register("title")}
+          required
+        />
+      </section>
 
-  return (
-    <div>
-      <form
-        className="flex flex-col gap-6 mb-52"
-        onSubmit={onSubmit(handleSubmit)}
-      >
+      <section>
+        <Label>Subtitle</Label>
+        <Input
+          type="text"
+          placeholder="Subtitle"
+          {...register("subtitle")}
+          required
+        />
+      </section>
+
+      <section>
+        <Label>Thumbnail</Label>
+
+        <Input
+          className="mb-2"
+          type="file"
+          {...register("thumbnail")}
+          accept="image/*"
+          onChange={(e) => handleThumbNailChange(e)}
+        />
+
+        <Input
+          type="text"
+          placeholder="Thumbnail alt"
+          {...register("thumbnail_alt")}
+          required
+        />
+      </section>
+      {image && (
         <section>
-          <Label>Title</Label>
-          <Input
-            type="text"
-            placeholder="Title"
-            {...register("title")}
-            required
-          />
+          <Image alt="thumbnail preview" width={100} height={100} src={image} />
         </section>
+      )}
 
-        <section>
-          <Label>Subtitle</Label>
-          <Input
-            type="text"
-            placeholder="Subtitle"
-            {...register("subtitle")}
-            required
-          />
-        </section>
-
-        <section>
-          <Label>Thumbnail</Label>
-
-          <Input
-            className="mb-2"
-            type="file"
-            {...register("thumbnail")}
-            accept="image/*"
-            onChange={(e) => handleThumbNailChange(e)}
-          />
-
-          <Input
-            type="text"
-            placeholder="Thumbnail alt"
-            {...register("thumbnail_alt")}
-            required
-          />
-        </section>
-        {image && (
-          <section>
-            <Image
-              alt="thumbnail preview"
-              width={100}
-              height={100}
-              src={image}
+      <section>
+        <Label>Content</Label>
+        <Controller
+          name="content"
+          control={control}
+          render={({ field }) => (
+            <Editor
+              apiKey={process.env.NEXT_PUBLIC_TINY_MCE_API}
+              id="my-custom-editor-id"
+              init={editorConfig}
+              onInit={(e, editor) => (editorRef.current = editor)}
+              onEditorChange={(newValue) => {
+                field.onChange(newValue);
+              }}
+              value={getValues("content")}
             />
-          </section>
-        )}
+          )}
+        />
+      </section>
 
-        <section>
-          <Label>Content</Label>
-          <Controller
-            name="content"
-            control={control}
-            render={({ field }) => (
-              <Editor
-                apiKey={process.env.NEXT_PUBLIC_TINY_MCE_API}
-                id="my-custom-editor-id"
-                init={editorConfig}
-                onInit={(e, editor) => (editorRef.current = editor)}
-                onEditorChange={(newValue) => {
-                  field.onChange(newValue);
-                }}
-                value={getValues("content")}
-              />
-            )}
-          />
-        </section>
-
-        <section>
-          <Label>Category</Label>
+      <section>
+        <Label>Category</Label>
+        {categories && (
           <Controller
             name="category_id"
             control={control}
             render={({ field }) => (
               <Select
-                value={field.value}
+                value={field.value.toString()}
                 onValueChange={(value) => {
                   field.onChange(value);
                   // 선택된 category_id에 해당하는 category_name 찾기
@@ -258,44 +257,46 @@ const PostForm = ({ post, lang }: { post?: IPost; lang: string }) => {
               </Select>
             )}
           />
-        </section>
+        )}
+      </section>
 
-        <section>
-          <Label>Status</Label>
-          <Controller
-            name="status"
-            control={control}
-            render={({ field }) => (
-              <Select
-                value={field.value}
-                onValueChange={(value) => field.onChange(value)}
-              >
-                <SelectTrigger>
-                  <SelectValue placeholder="Set Category" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectLabel>Status</SelectLabel>
-                    <SelectItem value="PUBLIC">Public</SelectItem>
-                    <SelectItem value="PRIVATE">Private</SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-            )}
-          />
-        </section>
+      <section>
+        <Label>Status</Label>
+        <Controller
+          name="status"
+          control={control}
+          render={({ field }) => (
+            <Select
+              value={field.value}
+              onValueChange={(value) => field.onChange(value)}
+            >
+              <SelectTrigger>
+                <SelectValue placeholder="Set Category" />
+              </SelectTrigger>
 
-        <section>
-          <Label>Tags</Label>
-          <HashContainer />
-        </section>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Status</SelectLabel>
+                  <SelectItem value="PUBLIC">Public</SelectItem>
+                  <SelectItem value="PRIVATE">Private</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          )}
+        />
+      </section>
 
-        <Button type="submit" disabled={isSubmitting}>
-          {lang} version
-        </Button>
-      </form>
-    </div>
-  );
+      <section>
+        <Label>Tags</Label>
+        <HashContainer />
+      </section>
+
+      <Button type="submit" disabled={isSubmitting}>
+        {lang} version
+      </Button>
+    </form>
+  </div>
+);
 };
 
 export default PostForm;
