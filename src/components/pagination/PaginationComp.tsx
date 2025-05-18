19b7a5b34cd1938ab973d2f68899e012a4196558
Hur Hyeon Bin (Max) <160996936+hyeonbinHur@users.noreply.github.com>
2025-05-19 03:02:09 +0900

    Prefecth pagination page translate 4 -> 186ms

--- src/components/pagination/PaginationComp.tsx
+++ src/components/pagination/PaginationComp.tsx
@@ -3,135 +3,95 @@ import React, { useEffect, useState } from "react";
 
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { Button } from "../ui/button";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useSearchParams } from "next/navigation";
+import Link from "next/link";
 
 const PaginationComp = ({ totalCount }: { totalCount: number }) => {
-  // limit = 7
-  // 현재 페이지 기준 뒤로 몇개의 아이템이 남았는지 계산
-  // 뒤에 몇개의 페이지가 더 존재할 수 있는지 확인?
-  /**
-   * 번호는 5개씩
-   * features, prev, <counts>, next
-   *  - prev allows move to prev page (current page - 1)
-   *  - counts allows navigate to 'count' page
-   *  - next allows naviage to next page (current page + 1)
-   *
-   * case 1. 7 is the last page number(count) <1, 2, 3, 4, 5, 6, 7>
-   * - when current page is 1
-   *   shows 1, 2, 3, 4, 5
-   *   prev is not allowed
-   *   next is allowed
-   *
-   * - when current page is 4
-   *   shows 3, 4 ,5 ,6, 7
-   *   prev is allowed
-   *   next is allowed
-   *
-   * - when current page is 7
-   *   shows 3,4,5,6,7
-   *   prev is allowed
-   *   next is not allowed
-   *
-   * caee 2. 3 is the last page number(count)
-   * - when current page is 1
-   *   shows 1, 2, 3
-   *   prev is not allowed
-   *   next is allowed
-   *
-   * - when current page is 2
-   *   shows 1, 2, 3
-   *   prev is allowed
-   *   next is allowed
-   *
-   * - when current page is 3
-   *   shows 1, 2, 3
-   *   prev is allowed
-   *   next is not allowed
-   *
-   * case 3. 1 is the last page number(count)
-   * - when current page is 1
-   *   - shows 1
-   *   - prev is not allowed
-   *   - next is not allowed
-   */
-
-  //Variable Declaration
-  const router = useRouter();
   const searchParams = useSearchParams();
 
-  const page = searchParams.get("page") || 1;
-  const currentPage = page ? parseInt(page as string) : 1;
+  const page = searchParams.get("page") || "1";
+  const currentPage = parseInt(page);
   const lastPage = Math.ceil(totalCount / 7);
 
   const [counts, setCounts] = useState<number[]>([]);
-  const [isNext, setIsNext] = useState<boolean>(true);
-  const [isPrev, setIsPrev] = useState<boolean>(true);
+  const [isNextDisabled, setIsNextDisabled] = useState(true);
+  const [isPrevDisabled, setIsPrevDisabled] = useState(true);
 
   useEffect(() => {
     if (lastPage > 5) {
       if (lastPage - currentPage >= 4) {
-        const results = Array.from(
-          { length: 5 },
-          (_, i) => i + currentPage + 1
-        );
+        const results = Array.from({ length: 5 }, (_, i) => currentPage + i);
         setCounts(results);
       } else {
-        const results = Array.from({ length: 5 }, (_, i) => lastPage - i);
-        setCounts(results.reverse());
+        const results = Array.from({ length: 5 }, (_, i) => lastPage - 4 + i);
+        setCounts(results);
       }
     } else {
       const results = Array.from({ length: lastPage }, (_, i) => i + 1);
       setCounts(results);
     }
-    if (currentPage !== lastPage) {
-      setIsNext(false);
-    }
-    if (currentPage !== 1) {
-      setIsPrev(false);
-    }
-  }, [page]);
+    setIsNextDisabled(currentPage === lastPage);
+    setIsPrevDisabled(currentPage === 1);
+  }, [currentPage, lastPage]);
 
-  //Client Component Event Handler
-  const onClickMovePage = (page: number) => {
-    window.dispatchEvent(new Event("routeChangeStartCustom"));
+  // 현재 페이지를 제외한 모든 페이지에 page 쿼리 붙이기 함수
+  const createPageLink = (pageNum: number) => {
     const params = new URLSearchParams(searchParams.toString());
-    params.set("page", page.toString());
-    router.push(`?${params.toString()}`);
+    params.set("page", pageNum.toString());
+    return `?${params.toString()}`;
   };
 
   return (
     <div className="my-5 w-full flex justify-center">
       {totalCount > 0 && (
-        <div className="flex">
+        <div className="flex items-center">
           <Button
-            disabled={isPrev}
+            asChild
+            disabled={isPrevDisabled}
             variant="outline"
             className="rounded-full w-10 h-10"
-            onClick={() => onClickMovePage(currentPage - 1)}
           >
-            <ChevronLeft />
+            <Link
+              href={createPageLink(currentPage - 1)}
+              aria-disabled={isPrevDisabled}
+              prefetch={true}
+            >
+              <ChevronLeft />
+            </Link>
           </Button>
-          <div className="flex text-slate-500">
-            {counts.map((e, i) => (
+
+          <div className="flex text-slate-500 mx-2">
+            {counts.map((pageNum) => (
               <button
-                key={i}
-                onClick={() => onClickMovePage(e as number)}
+                key={pageNum}
                 className={`w-10 h-10 flex items-center justify-center hover:text-black ${
-                  currentPage === e ? `text-black font-bold` : ``
+                  currentPage === pageNum ? "text-black font-bold" : ""
                 }`}
               >
-                {e}
+                <Link
+                  href={createPageLink(pageNum)}
+                  aria-current={currentPage === pageNum ? "page" : undefined}
+                  prefetch={true}
+                >
+                  {pageNum}
+                </Link>
               </button>
             ))}
           </div>
 
           <Button
-            disabled={isNext}
+            asChild
+            disabled={isNextDisabled}
             variant="outline"
             className="rounded-full w-10 h-10"
-            onClick={() => onClickMovePage(currentPage + 1)}
           >
-            <ChevronRight />
+            <Link
+              href={createPageLink(currentPage + 1)}
+              aria-disabled={isNextDisabled}
+              prefetch={true}
+            >
+              <ChevronRight />
+            </Link>
           </Button>
         </div>
       )}
